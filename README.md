4cbb0cfdf94d617980542fc067b986942bcaf3cc
pedoc <pedoc@qq.com>
2022-05-21 12:27:54 +0800

    rename Readme.md to README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+## 说明
+提供一些小工具
+
+1. ``pwd`` 等同于linux下的pwd,但是支持windows,且输出更友好
+![img.png](assets/img.png)
+
+2. ``opendef`` 支持在指定目录中搜索指定扩展名的文件并使用系统默认程序打开(如果找到多个同名文件,仅打开第一个)
+命令参考
+```
+opendef E:\OpenSource\minitools md  //表示在 E:\OpenSource\minitools 目录中搜索扩展名为 md 的文件并自动使用markdown编辑器打开
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
-use std::path::Path;
+use std::ffi::OsStr;
+use std::fmt::Debug;
+use std::path::{Path, PathBuf};
 
-pub mod common {
-    pub fn path_exists(path: &str) -> bool {
-        Path::new(path).exists()
+pub fn path_exists(path: &str) -> bool {
+    Path::new(path).exists()
+}
+
+pub fn filter_files(dir: &str, ext: &str) -> Vec<PathBuf> {
+    let mut faxvec: Vec<PathBuf> = Vec::new();
+    for element in std::path::Path::new(dir).read_dir().unwrap() {
+        let path = element.unwrap().path();
+        if let Some(extension) = path.extension() {
+            if extension == ext {
+                faxvec.push(path);
+            }
+        }
     }
+    return faxvec;
 }
